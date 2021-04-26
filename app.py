247e1369f98864a6b0b4c76376a19f950570c37b
Ayaan Imran <miskiacuberayaan2509@gmail.com>
2021-04-26 16:20:48 +0300

    Basic structure contructed

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import turtle
 import css
 
+# Functions and global variables
+ENDCODED_FILE = []
+
 # Greet
-print("Hi. Welcome to Turtle Nursery")
+print("Hi. Welcome to Turtle Nursery where you can create python turtle files without writing a single line of code")
+print()
+
+# The main part
+while True:
+    pass
