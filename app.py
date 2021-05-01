ded4958d050a6b304bfbfc42422e1102ff4d8b27
Ayaan Imran <miskiacuberayaan2509@gmail.com>
2021-05-01 14:18:11 +0300

    encode() function added
    Now there is an encode function that will turn the DECODED_FILE to ENCODED_FILE, which is the user's code

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import turtle
 import css
 
 # Functions and global variables
-ENDCODED_FILE = []
+ENCODED_FILE = []
+DECODED_FILE = []
 
+# Decodes it to python turtle language
 def decode():
     """
     This function will convert the user's code, which is in the ENCODED_FILE variable, to python
@@ -14,7 +16,7 @@ def decode():
     DECODED_FILE = []
 
     # Loop through each command in the ENCODED_FILE
-    for i in ENDCODED_FILE:
+    for i in ENCODED_FILE:
         first_part = i.split()[0]
         second_part = i.split()[1]
 
@@ -33,12 +35,12 @@ def decode():
 
         elif first_part == "pen":
             if second_part == "up":
-                decoded_line = "my_turtle.pen_up()"
+                decoded_line = "my_turtle.penup()"
 
                 DECODED_FILE.append(decoded_line)
 
             if second_part == "down":
-                decoded_line = "my_turtle.pen_down()"
+                decoded_line = "my_turtle.pendown()"
 
                 DECODED_FILE.append(decoded_line)
 
@@ -55,6 +57,55 @@ def decode():
                 DECODED_FILE.append(decoded_line)
 
     return DECODED_FILE
+
+# Encodes it to user's code
+def encode():
+    """
+    This function will convert the python turtle code, which is in the DECODED_FILE variable, to user's
+    code and store it in the ENCODED_FILE variable
+    """
+
+    global ENCODED_FILE
+
+    # Clear the encoded file variable
+    ENCODED_FILE.clear()
+
+    # Loop through each command in the DECODED_FILE
+    for i in DECODED_FILE: # i = my_turtle.forward(20)
+        main_line = i.split(".")[1]  # Remove the 'my_turtle.' part
+
+        command = main_line.split("(")[0] # Gets the command: Example forward
+
+        # This part gets the steps
+        steps = ""
+        for i in main_line:
+            numbers = [f"{i}" for i in range(0, 11)] # Creates numbers: "1", "2", "3", "0" ...
+
+            if i in numbers: # Checks if i is a number
+                steps += i # Appends this to the steps variable
+
+        if steps == "": # Cleans the steps. If command is penup, that means steps is None
+            steps = None
+        else:
+            steps = int(steps)
+
+        # Convert to user's code
+        if (command == "forward") or (command == "backward"):
+            user_code = f"move {command} {steps} steps"
+
+        elif command == "penup":
+            user_code = "pen up"
+
+        elif command == "pendown":
+            user_code = "pen down"
+
+        elif (command == "left") or (command == "right"):
+            user_code = f"turn {command} {steps} degrees"
+
+        # Append it to the ENCODED_FILE
+        ENCODED_FILE.append(user_code)
+
+    return ENCODED_FILE
 
 # Greet
 print("Hi. Welcome to Turtle Nursery where you can create python turtle files without writing a single line of code")
